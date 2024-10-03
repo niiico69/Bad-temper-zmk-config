ff857110dec40b82507ae3d0be32e99ad8d65f23
niiico69 <niiico@gmail.com>
2024-10-03 09:17:10 +0200

    Temper config to be able to type by default with the system layout. By selectioning Bepolar or Ergo-L, you can type with these layout without changing the layout in the OS. Based on https://github.com/JeffDess/zmk-config/ and https://github.com/urob/zmk-helpers/

---
+++ boards/shields/temper/constants.h
@@ -0,0 +1,36 @@
+#pragma once
+
+// Keys
+#define ___ &trans
+#define XXX &none
+
+// Layers
+// Must match order in which they are defined
+#define QWE 0
+#define ERG 1
+#define BEP 2
+#define SYM 3
+#define DKE 4
+#define DKB 5
+#define NAV 6
+#define QNV 7
+#define MAC 8
+#define ADJ 9
+
+// Modifier keys
+#define GL LGUI
+#define CL LCTRL
+#define AL LALT
+#define SL LSHFT
+#define GR RGUI
+#define CR RCTRL
+#define AR RALT
+#define SR RSHFT
+
+// OS
+#define WINDOWS 0
+#define LINUX 1
+#define MAC_OS 2
+
+// Apps
+#define TMUX LC(LA(J)) // Tmux -> Ctrl + Alt + J
