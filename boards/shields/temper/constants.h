d6b0ba75815cd270c5ce418d48707fbb2d2ca3e4
niiico69 <niiico@gmail.com>
2024-10-05 14:54:01 +0200

    Clavier bepolar sur azerty

---
+++ boards/shields/temper/constants.h
@@ -0,0 +1,34 @@
+#pragma once
+
+// Keys
+#define ___ &trans
+#define XXX &none
+
+// Layers
+// Must match order in which they are defined
+#define ERG 0
+#define BEP 1
+#define SYM 2
+#define DKE 3
+#define DKB 4
+#define NAV 5
+#define MAC 6
+#define ADJ 7
+
+// Modifier keys
+#define GL LGUI
+#define CL LCTRL
+#define AL LALT
+#define SL LSHFT
+#define GR RGUI
+#define CR RCTRL
+#define AR RALT
+#define SR RSHFT
+
+// OS
+#define WINDOWS 0
+#define LINUX 1
+#define MAC_OS 2
+
+// Apps
+#define TMUX LC(LA(J)) // Tmux -> Ctrl + Alt + J
